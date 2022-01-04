c2a87dd890e30695df751059e9f75f1ed69f88f8
Guido W. Pettinari <guido.pettinari@idearia.it>
2022-01-04 13:59:28 +0100

    CrabadaWeb2Client: endpoint listCrabsForLending

--- src/libs/CrabadaWeb2Client/CrabadaWeb2Client.py
+++ src/libs/CrabadaWeb2Client/CrabadaWeb2Client.py
@@ -25,6 +25,11 @@ class CrabadaWeb2Client:
         return requests.request("GET", url, params=params).json()
 
     def listMines(self, params: dict[str, Any] = {}) -> List[CrabadaGame]:
+        """Get all mines.
+        
+        If you want only the open mines, pass status=open in the params.
+        If you want only a certain user's mines, use the user_address param.
+        """
         res = self.listMines_Raw(params)
         try:
             return res['result']['data'] or []
@@ -32,11 +37,6 @@ class CrabadaWeb2Client:
             return []
 
     def listMines_Raw(self, params: dict[str, Any] = {}) -> Any:
-        """Get all mines.
-        
-        If you want only the open mines, pass status=open in the params.
-        If you want only a certain user's mines, use the user_address param.
-        """
         url = self.baseUri + '/mines'
         defaultParams = {
             "limit": 5,
@@ -46,13 +46,6 @@ class CrabadaWeb2Client:
         return requests.request("GET", url, params=actualParams).json()
 
     def listTeams(self, userAddress: Address, params: dict[str, Any] = {}) -> List[CrabadaTeam]:
-        res = self.listTeams_Raw(userAddress, params)
-        try:
-            return res['result']['data'] or []
-        except:
-            return []
-
-    def listTeams_Raw(self, userAddress: Address, params: dict[str, Any] = {}) -> Any:
         """Get all teams of a given user address.
         
         If you want only the available teams, pass is_team_available=1
@@ -60,6 +53,13 @@ class CrabadaWeb2Client:
         It is currently not possible to list all users' teams, you can
         only see the teams of a specific user.
         """
+        res = self.listTeams_Raw(userAddress, params)
+        try:
+            return res['result']['data'] or []
+        except:
+            return []
+
+    def listTeams_Raw(self, userAddress: Address, params: dict[str, Any] = {}) -> Any:
         url = self.baseUri + '/teams'
         defaultParams = {
             "limit": 5,
@@ -67,4 +67,25 @@ class CrabadaWeb2Client:
         }
         actualParams = defaultParams | params
         actualParams['user_address'] = userAddress
+        return requests.request("GET", url, params=actualParams).json()
+
+    def listCrabsForLending(self, params: dict[str, Any] = {}) -> Any:
+        """Get all crabs available for lending as reinforcements; you can use
+        sortBy and sort parameters, default is orderBy": 'price' and
+        "order": 'asc'"""
+        res = self.listCrabsForLending_Raw(params)
+        try:
+            return res['result']['data'] or []
+        except:
+            return []
+
+    def listCrabsForLending_Raw(self, params: dict[str, Any] = {}) -> Any:
+        url = self.baseUri + '/crabadas/lending'
+        defaultParams = {
+            "limit": 10,
+            "page": 1,
+            "orderBy": 'price',
+            "order": 'asc',
+        }
+        actualParams = defaultParams | params
         return requests.request("GET", url, params=actualParams).json()
---
+++ src/libs/CrabadaWeb2Client/Tests/testListCrabsForLending.py
@@ -0,0 +1,20 @@
+from src.libs.CrabadaWeb2Client.CrabadaWeb2Client import CrabadaWeb2Client
+from pprint import pprint
+
+# VARS
+client = CrabadaWeb2Client()
+
+# TEST FUNCTIONS
+def testListCrabsForLendingByPrice() -> None:
+    params = {"limit": 3, "page": 1, "orderBy": "price", "order": "asc"}
+    pprint(client.listCrabsForLending(params=params))
+
+def testListCrabsForLendingByMinePoints() -> None:
+    params = {"limit": 3, "page": 1, "orderBy": "mine_point", "order": "desc"}
+    pprint(client.listCrabsForLending(params=params))
+
+# EXECUTE
+print(">>> REINFORCEMENTS - CHEAPEST FIRST")
+testListCrabsForLendingByPrice()
+print(">>> REINFORCEMENTS - HIGHEST MINE POINTS FIRST")
+testListCrabsForLendingByMinePoints()
