90693960bf38444dba43a4b835dcf5e6998f73b6
Guido W. Pettinari <guido.pettinari@idearia.it>
2022-01-19 12:37:27 +0100

    Adjusted comments

--- src/libs/Web3Client/AvalancheWeb3Client.py
+++ src/libs/Web3Client/AvalancheWeb3Client.py
@@ -3,14 +3,18 @@ from src.libs.Web3Client.Web3Client import Web3Client
 from web3.middleware import geth_poa_middleware
 
 class AvalancheWeb3Client(Web3Client):
-    """Client to interact with the Avalanche blockchain and
-    its smart contracts."""
+    """
+    Client to interact with the Avalanche blockchain and
+    its smart contracts.
+    """
 
     maxPriorityFeePerGasInGwei: int = 2 # TODO: fine tune
     gasLimit: int = 400000 # sensible value for Avalanche
 
     def setNodeUri(self, nodeUri: str) -> AvalancheWeb3Client:
-        """Inject the POA middleware"""
+        """
+        Inject the POA middleware
+        """
         super().setNodeUri(nodeUri)
         self.w3.middleware_onion.inject(geth_poa_middleware, layer=0)
         return self
--- src/libs/Web3Client/Web3Client.py
+++ src/libs/Web3Client/Web3Client.py
@@ -5,15 +5,17 @@ from eth_typing import Address
 from web3 import Web3
 from eth_account.datastructures import SignedTransaction
 from web3.contract import Contract, ContractFunction
-from web3.types import Nonce, TxParams, TxReceipt
+from web3.types import BlockData, Nonce, TxParams, TxReceipt
 from eth_typing.encoding import HexStr
 
 class Web3Client:
-    """Client to interact with a blockchain, with smart
+    """
+    Client to interact with a blockchain, with smart
     contract support.
     
     Wrapper of the Web3 library intended to make it easier
-    to use."""
+    to use.
+    """
 
     maxPriorityFeePerGasInGwei: int = None
     gasLimit: int = None    
@@ -32,11 +34,13 @@ class Web3Client:
     ####################
 
     def buildBaseTransaction(self) -> TxParams:
-        """Build a basic EIP-1559 transaction with just nonce, chain ID and gas;
+        """
+        Build a basic EIP-1559 transaction with just nonce, chain ID and gas;
         you should already have called setChainId() and setNodeUri.
         
         Gas is estimated according to the formula
-        maxMaxFeePerGas = 2 * baseFee + maxPriorityFeePerGas."""
+        maxMaxFeePerGas = 2 * baseFee + maxPriorityFeePerGas.
+        """
         tx: TxParams = {
             'type': 0x2,
             'chainId': self.chainId,
@@ -48,17 +52,21 @@ class Web3Client:
         return tx
     
     def buildTransactionWithValue(self, to: Address, valueInEth: float) -> TxParams:
-        """Build a transaction involving a transfer of value to an address,
-        where the value is expressed in the blockchain token (e.g. ETH or AVAX)."""
+        """
+        Build a transaction involving a transfer of value to an address,
+        where the value is expressed in the blockchain token (e.g. ETH or AVAX).
+        """
         tx = self.buildBaseTransaction()
         txValue: TxParams = { 'to': to, 'value': self.w3.toWei(valueInEth, 'ether') }
         return tx | txValue
 
     def buildContractTransaction(self, contractFunction: ContractFunction) -> TxParams:
-        """Build a transaction that involves a contract interation.
+        """
+        Build a transaction that involves a contract interation.
         
         Requires passing the contract function as detailed in the docs:
-        https://web3py.readthedocs.io/en/stable/web3.eth.account.html#sign-a-contract-transaction"""
+        https://web3py.readthedocs.io/en/stable/web3.eth.account.html#sign-a-contract-transaction
+        """
         baseTx = self.buildBaseTransaction()
         return contractFunction.buildTransaction(baseTx)
 
@@ -67,24 +75,31 @@ class Web3Client:
     ####################
 
     def signTransaction(self, tx: TxParams) -> SignedTransaction:
-        """Sign the give transaction; the private key must have
+        """
+        Sign the give transaction; the private key must have
         been set with setCredential().
         """
         return self.w3.eth.account.sign_transaction(tx, self.privateKey)
 
     def sendSignedTransaction(self, signedTx: SignedTransaction) -> HexStr:
-        """Send a signed transaction and return the tx hash"""
+        """
+        Send a signed transaction and return the tx hash
+        """
         tx_hash = self.w3.eth.send_raw_transaction(signedTx.rawTransaction)
         return self.w3.toHex(tx_hash)
 
     def signAndSendTransaction(self, tx: TxParams) -> HexStr:
-        """Sign a transaction and send it"""
+        """
+        Sign a transaction and send it
+        """
         signedTx = self.signTransaction(tx)
         return self.sendSignedTransaction(signedTx)
     
     def getTransactionReceipt(self, txHash: HexStr) -> TxReceipt:
-        """Given a transaction, wait for the blockchain to confirm
-        it and return the tx receipt."""
+        """
+        Given a transaction, wait for the blockchain to confirm
+        it and return the tx receipt.
+        """
         return self.w3.eth.wait_for_transaction_receipt(txHash)
 
     ####################
@@ -95,24 +110,40 @@ class Web3Client:
         return self.w3.eth.get_transaction_count(self.userAddress)
 
     def estimateMaxFeePerGasInGwei(self) -> int:
-        """Gets the base fee from the latest block and returns a maxFeePerGas
+        """
+        Gets the base fee from the latest block and returns a maxFeePerGas
         estimate as 2 * baseFee + maxPriorityFeePerGas, as done in the
         web3 gas_price_strategy middleware (and also here >
-        https://ethereum.stackexchange.com/a/113373/89782)"""
+        https://ethereum.stackexchange.com/a/113373/89782)
+        """
         latest_block = self.w3.eth.get_block('latest')
         baseFeeInWei = latest_block['baseFeePerGas'] # in wei
         baseFeeInGwei = int(Web3.fromWei(baseFeeInWei, 'gwei'))
         return 2 * baseFeeInGwei + self.maxPriorityFeePerGasInGwei
+
+    def getLatestBlock(self) -> BlockData:
+        """
+        Return the latest block
+        """
+        return self.w3.eth.get_block('latest')
+
+    def getPendingBlock(self) -> BlockData:
+        """
+        Return the pending block
+        """
+        return self.w3.eth.get_block('pending')
 
     ####################
     # Setters
     ####################
 
     def setContract(self, contractAddress: Address, abiFile: str) -> Web3Client:
-        """Load the smart contract, required before running
+        """
+        Load the smart contract, required before running
         buildContractTransaction().
 
-        Run only after setting the node URI (setNodeUri)"""
+        Run only after setting the node URI (setNodeUri)
+        """
         self.contractAddress = contractAddress
         self.contractChecksumAddress = Web3.toChecksumAddress(contractAddress)
         self.abi = self.__getContractAbi(abiFile) # Read the contract's ABI from a JSON file
