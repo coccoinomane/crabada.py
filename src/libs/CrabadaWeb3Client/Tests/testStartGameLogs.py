0811bf41d2e55d62dfb2fe2d626dcd2fdd1f6a68
Guido W. Pettinari <guido.pettinari@idearia.it>
2022-01-21 20:53:10 +0100

    Rename test

--- src/libs/CrabadaWeb3Client/Tests/testStartGameLogs.py
+++ src/libs/CrabadaWeb3Client/Tests/testStartGameLogs.py
@@ -6,14 +6,13 @@ Docs: https://web3py.readthedocs.io/en/stable/contracts.html#events
 """
 
 from typing import cast
-from src.libs.Web3Client.helpers.debug import printTxInfo
+from src.libs.Web3Client.helpers.debug import pprintAttributeDict
 from src.helpers.general import secondOrNone
-from src.common.config import nodeUri, users, contract, chainId
+from src.common.config import nodeUri, contract, chainId
 from src.libs.CrabadaWeb3Client.CrabadaWeb3Client import CrabadaWeb3Client
 from sys import argv
 from eth_typing.encoding import HexStr
 from pprint import pprint
-from web3 import Web3
 
 # VARS
 txHash = cast(HexStr, (secondOrNone(argv) or "0x41705baf18b1ebc8ec204926a8524d3530aada11bd3c249ca4a330ed047f005e"))
@@ -32,7 +31,8 @@ def testContractEvents() -> None:
     print(">>> TX")
     pprint(tx)
     print(">>> TX LOGS")
-    pprint(logs)
+    for log in logs:
+        pprintAttributeDict(log)
 
 # EXECUTE
 testContractEvents()
