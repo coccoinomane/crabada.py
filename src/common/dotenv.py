9b28ac6f93c636ea0d3e7dbc1f0b1f0beed8b15a
Guido W. Pettinari <guido.pettinari@idearia.it>
2022-03-04 18:12:49 +0100

    Raise error if no .env is found

--- src/common/dotenv.py
+++ src/common/dotenv.py
@@ -1,7 +1,10 @@
 import os, dotenv
 from typing import Any
+from src.common.exceptions import MissingConfig
 
 # Load environment from .env
+if (not os.path.isfile('.env')):
+    raise MissingConfig(".env file not found")
 dotenv.load_dotenv()
 
 # Shorthand to get environment variables
