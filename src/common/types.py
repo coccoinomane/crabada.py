af3a130f8ce751cc0fe40b4d9fcf5a4796809758
Guido W. Pettinari <guido.pettinari@idearia.it>
2022-03-06 18:46:17 +0100

    TUS now a float

--- src/common/types.py
+++ src/common/types.py
@@ -2,7 +2,7 @@ from typing import Literal, NewType, TypedDict, List
 from eth_typing import Address
 from web3.types import Wei
 
-Tus = NewType('Tus', int)
+Tus = NewType('Tus', float)
 
 """
 Task assigned to a team
