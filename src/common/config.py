5166743c4a6ffdc39820a1d4a30db3a5e5a43310
yigiter.yigit <yigiteryigiter@gmail.com>
2022-03-26 14:36:03 +0300

    Add instant_messaging (telegram) helpers.

--- src/common/config.py
+++ src/common/config.py
@@ -52,10 +52,19 @@ twilio = {
     "authToken": getenv("TWILIO_AUTH_TOKEN"),
 }
 
+telegram = {
+    "enable": True if "1" == str(getenv("TELEGRAM_ENABLE", "1")) else False,
+    "apiKey": getenv("TELEGRAM_API_KEY"),
+    "chatId": getenv("TELEGRAM_CHAT_ID"),
+}
+
 notifications = {
     "sms": {
         "enable": True if "1" == str(getenv("NOTIFICATION_SMS", "0")) else False,
         "from": getenv("NOTIFICATION_SMS_FROM"),
         "to": getenv("NOTIFICATION_SMS_TO"),
-    }
+    },
+    "instantMessage": {
+        "enable": True if "1" == str(getenv("NOTIFICATION_IM", "0")) else False,
+    },
 }
