999729e1a7fef86d3a825ffe35731e08f9ff16e2
coccoinomane <coccoinomane@gmail.com>
2022-05-04 20:32:03 +0200

    Split run.py script in bin + bot folders

---
+++ bin/mining/run.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Crabada script to continuosly mine, reinforce and claim rewards
+for all mining teams of the given user.
+
+Usage:
+    python3 -m bin.mining.run
+"""
+
+from typing import cast
+from eth_typing import Address
+from src.bot.mining.run import run
+from src.common.dotenv import getenv, parseInt
+from src.models.User import User
+from src.common.logger import logger
+from sys import exit
+
+# Use USER_1_ADDRESS as userAddress parameter
+userAddress = cast(Address, getenv("USER_1_ADDRESS"))
+
+# Like a cronjob definiton
+sleep_timer = parseInt("SLEEP_TIMER", 120)
+
+# We usually can't call sendTeamsMining immediately after closeMines
+# but waiting for full 120 seconds a bit too much.
+# introduced a second sleep parameter for this reason.
+sleep_timer_minor = parseInt("SLEEP_TIMER_MINOR", 20)
+
+if not userAddress:
+    logger.error("Specify a user address")
+    exit(1)
+
+run(User(userAddress), sleep_timer, sleep_timer_minor)
--- src/bot/mining/run.py
+++ src/bot/mining/run.py
@@ -1,59 +1,47 @@
 from time import sleep
+from typing import cast
+from eth_typing import Address
 from src.bot.mining.sendTeamsMining import sendTeamsMining
 from src.bot.mining.reinforceDefense import reinforceDefense
 from src.bot.mining.closeMines import closeMines
-
 from src.common.dotenv import getenv, parseInt
 from src.models.User import User
 from src.common.logger import logger
 from sys import exit
 
-# use USER_1_ADDRESS as userAddress parameter
-userAddress = getenv("USER_1_ADDRESS")
 
-# Like a cronjob definiton,
-sleep_timer = parseInt("SLEEP_TIMER", 120)
+def run(user: User, sleep_timer: int, sleep_timer_minor: int) -> None:
+    """
+    Infinite loop that periodically sends available teams mining,
+    reinforces them and eventually claimes their rewards, and then
+    starts over again.
+    """
 
-# we usually can't call sendTeamsMining immediately after closeMines
-# but waiting for full 120 seconds a bit too much.
-# introduced a second sleep parameter for this reason.
-sleep_timer_minor = parseInt("SLEEP_TIMER_MINOR", 20)
+    while True:
+        try:
+            nReinforced = reinforceDefense(user)
+        except:
+            logger.exception("reinforceDefense exception at run.py")
 
-if not userAddress:
-    logger.error("Specify a user address")
-    exit(1)
+        try:
+            nClosed = closeMines(user)
+            if nClosed > 0:
+                logger.debug(
+                    f"Closed mine count is {nClosed}, waiting for {sleep_timer_minor}"
+                )
+                sleep(sleep_timer_minor)
+        except:
+            logger.exception("closeMines exception at run.py")
 
-if not User.isRegistered(userAddress):
-    logger.error("The given user address is not registered")
-    exit(1)
+        try:
+            nSent = sendTeamsMining(user)
+            if nSent > 0:
+                logger.debug(
+                    f"Started mine count is {nSent}, waiting for {sleep_timer_minor}"
+                )
+                sleep(sleep_timer_minor)
+        except:
+            logger.exception("sendTeamsMining exception at run.py")
 
-
-while True:
-
-    try:
-        nReinforced = reinforceDefense(User(userAddress))
-    except:
-        logger.exception("reinforceDefense exception at run.py")
-
-    try:
-        nClosed = closeMines(User(userAddress))
-        if nClosed > 0:
-            logger.debug(
-                f"Closed mine count is {nClosed}, waiting for {sleep_timer_minor}"
-            )
-            sleep(sleep_timer_minor)
-    except:
-        logger.exception("closeMines exception at run.py")
-
-    try:
-        nSent = sendTeamsMining(User(userAddress))
-        if nSent > 0:
-            logger.debug(
-                f"Started mine count is {nSent}, waiting for {sleep_timer_minor}"
-            )
-            sleep(sleep_timer_minor)
-    except:
-        logger.exception("sendTeamsMining exception at run.py")
-
-    logger.debug(f"Waiting for {sleep_timer} seconds.")
-    sleep(sleep_timer)
+        logger.debug(f"Waiting for {sleep_timer} seconds.")
+        sleep(sleep_timer)
