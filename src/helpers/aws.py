78be1eab9420b78841b4da705c332dd603137635
Vladimir Ulogov <vladimir.ulogov@me.com>
2022-05-09 20:10:35 -0600

    awssecrets tool

--- src/helpers/aws.py
+++ src/helpers/aws.py
@@ -3,9 +3,11 @@ AWS services helper classes and functions for Crabada project
 """
 
 import boto3
+from src.common.logger import logger
 
 class SecretsManager:
     def __init__(self):
+        logger.debug("Initializing AWS SN client")
         self.client = boto3.client("secretsmanager")
 
     def __del__(self):
@@ -15,8 +17,10 @@ class SecretsManager:
         try:
             sec = self.get_secret_value(SecretId=name)
         except:
+            logger.debug("Creating AWS SM secret: %s"%name)
             self.client.create_secret(Name=name, Description=desc, SecretString=value, ForceOverwriteReplicaSecret=True)
             return
+        logger.debug("Creating AWS SM secret: %s"%name)
         self.client.put_secret_value(SecretId=name, SecretString=value)
 
     def __setitem__(self, key, val):
@@ -26,10 +30,19 @@ class SecretsManager:
         try:
             return self.client.get_secret_value(SecretId=key)
         except:
+            logger.error("AWS SM secret not found: %s"%name)
             raise KeyError(key)
 
-    def __delitem__(self, key):
+    def __delitem__(self, name):
         try:
-            self.client.delete_secret(SecretId=key)
+            logger.debug("Removing AWS SM secret: %s"%name)
+            self.client.delete_secret(SecretId=name)
         except:
+            logger.error("Handled error happens during removal of AWS SM secret: %s"%name)
             pass
+
+    def keys(self):
+        res = self.client.list_secrets(SortOrder="asc")
+        rsp = res["ResponseMetadata"]
+        logger.debug("AWS RequestId: %s"%rsp["RequestId"])
+        return res["SecretList"]
